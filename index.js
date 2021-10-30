21d1fcd793a679de01c7835afeea41d38979dd13
Erick Falconi <e.alexander.f013@gmail.com>
2021-10-30 10:58:34 -0400

    done part 1 - no bonus

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(path, pixelsLeft, pixelsBottom) {
+    let pictures = document.createElement('img')
+    pictures.src = path
+    pictures.style.position = 'fixed'
+    pictures.style.left = pixelsLeft
+    pictures.style.bottom = pixelsBottom
+    document.body.append(pictures)  
+}
+
+newImage('assets/tree.png', '200px', '300px' )
+newImage('assets/pillar.png', '350px', '100px' )
+newImage('assets/crate.png', '150px', '200px' )
+newImage('assets/well.png', '500px', '425px' )
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+
+
+function newItem(path, pixelsLeft2, pixelsBottom2) {
+    let item = document.createElement('img')
+    item.src = path
+    item.style.position = 'fixed'
+    item.style.left = pixelsLeft2
+    item.style.bottom = pixelsBottom2
+    document.body.append(item)
+
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
+}
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png','165px', '185px')
+newItem('assets/staff.png','600px', '100px')
